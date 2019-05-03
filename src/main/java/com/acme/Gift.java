db9059704c08425a2795b7f78b3337f930636406
jclingan <jclingan@redhat.com>
2019-05-03 13:11:01 -0500

    added restful operations

--- src/main/java/com/acme/Gift.java
+++ src/main/java/com/acme/Gift.java
@@ -1,14 +1,25 @@
 package com.acme;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 
 @Entity
-public class Gift {
+@NamedQueries({
+@NamedQuery(name = "Gift.findAll", query = "SELECT g FROM Gift g")
+, @NamedQuery(name = "Gift.findById", query = "SELECT g FROM Gift g WHERE g.id = :id")
+})
+
+public class Gift implements Serializable {
     private Long id;
     private String name;
+
+    public Gift() { }
 
     @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="giftSeq")
     public Long getId() {
--- src/main/java/com/acme/SantaClausService.java
+++ src/main/java/com/acme/SantaClausService.java
@@ -3,17 +3,53 @@ package com.acme;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @ApplicationScoped
+@Path("/api")
 public class SantaClausService {
     @Inject
-    EntityManager em; 
+    EntityManager em;
 
-    @Transactional 
-    public void createGift(String giftDescription) {
+    @Transactional
+    @POST
+    @Path("/{description}")
+    public void createGift(@PathParam("description") String giftDescription) {
         Gift gift = new Gift();
         gift.setName(giftDescription);
         em.persist(gift);
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Gift[] getAllGifts() {
+        return em.createNamedQuery("Gift.findAll", Gift.class).getResultList().toArray(new Gift[0]);
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{id}")
+    @Transactional
+    public void deleteGift(@PathParam("id") int id) {
+        Gift gift = null;
+        Query query = em.createNamedQuery("Gift.findById");
+        query.setParameter("id", (long)id);
+        try {
+            gift = (Gift) query.getSingleResult();
+        } catch (PersistenceException ex) {
+        }
+
+        if (gift != null) {
+            em.remove(gift);
+        }
     }
 }
